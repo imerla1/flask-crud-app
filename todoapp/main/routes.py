902b4e12444cf557d756ed4c777fda871deac677
imerla1 <giorgi1048@yahoo.com>
2021-07-13 13:05:34 +0400

    reamin finished

--- todoapp/main/routes.py
+++ todoapp/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, redirect, Blueprint
+from flask import render_template, url_for, redirect, Blueprint, request, jsonify, json
 from todoapp.models import Todo
 from todoapp import db
 
@@ -9,7 +9,41 @@ main = Blueprint("main", __name__)
 @main.route("/", methods=["GET", "POST"])
 @main.route("/index")
 def index():
-    t = Todo(data="Running")
+    t = Todo.query.all()
+    return render_template('index.html', data=t)
+
+
+@main.route("/add/<string:content>", methods=["POST", "GET"])
+def add(content):
+    cnt = str(content)
+    t = Todo(data=cnt)
     db.session.add(t)
     db.session.commit()
-    return "Index Page"
+    return redirect(url_for("main.index"))
+
+@main.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect(url_for('main.index'))
+    except:
+        return 'Error'
+
+@main.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.data = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        except:
+            return 'Error'
+
+    else:
+        return render_template('update.html', task=task)
